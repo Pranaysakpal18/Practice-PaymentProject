56486b49584d94a85c8e0bb58fc3acb75509af46
Pranaysakpal18 <138707719+Pranaysakpal18@users.noreply.github.com>
2026-06-07 15:06:04 +0530

    updated language with 22222

--- src/LanguageWrapper/LanguageWrapper.jsx
+++ src/LanguageWrapper/LanguageWrapper.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LanguageWrapper = () => {
   return (
     <div>
-        <h1>Im Working On language Wrapper</h1>
+        <h1>Im Working On language Wrapper222222222222</h1>
       
     </div>
   )
