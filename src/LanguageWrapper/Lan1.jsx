5108a323f7099eab55d2c2c8d469e1dd7def09d5
Pranaysakpal18 <138707719+Pranaysakpal18@users.noreply.github.com>
2026-06-07 15:19:59 +0530

    created Languages Component and updated

---
+++ src/LanguageWrapper/Lan1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Lan1 = () => {
+  return (
+    <div>
+        <h1>Im Working On language Wrapper111111111111</h1>
+      
+    </div>
+  )
+}
+
+export default Lan1
